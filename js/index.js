0f3d622c4d225b0772f03908163f342850a8ee38
Catherine Kawara <ndutakawara2@gmail.com>
2022-02-05 19:38:34 +0300

    chore:variable-declaration - - declare variables to help us keep track of the answers

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 function getResult(){
 var score = 0;
-var answer1 =document.game.question1.value;
-
+var answer1 = document.game.question1.value;
+var answer2 = document.game.question2.value;
+var answer3 = document.game.question3.value;
+var answer4 = document.game.question4.value;
+var answer5 = document.game.question5.value;
+var answer6 = document.game.question6.value;
 }
